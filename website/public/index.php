84cb20be2369cd0c242d184e7871e56fb3165a2e
Matt George <mattgeorge1@gmail.com>
2014-02-22 16:31:25 -0800

    add comments and clean path dependence to web pages

--- website/public/index.php
+++ website/public/index.php
@@ -1,7 +1,11 @@
 <?php
 
+// Set path to arxivPicks files
+$APHOME = "/path/to/arxivPicks"
+$db_file = $APHOME."/backend/arxivPicks.db";
+$logfile = $APHOME."/website/log.txt";
+
 // Log IP address
-$logfile = '/insert/path/to/log/log.txt';
 $IP = $_SERVER['REMOTE_ADDR'];
 $REF = $_SERVER['HTTP_REFERER'];
 date_default_timezone_set('America/Los_Angeles');
@@ -25,16 +29,12 @@ echo "</div>\n";
 echo "<div id=\"content\"><div id=\"abs\"><div class=\"subheader\">\n";
 echo "<h1><a href=\"http://arxiv.org/list/astro-ph/new?skip=0&show=250\">astro-ph</a> | <a href=\"old.php\">Old papers</a></h1>\n";
 echo "</div></div>\n";
-
 echo "<div id=\"dlpage\">\n";
 
-
-// set path of database file
-$db_file = "/insert/path/to/database/arxiv.db";
-// open database object
+// Open database object
 $db = new SQLiteDatabase($db_file) or die("Could not open database");
 
-
+// Design database query depending on date
 if($day_of_week == "Wednesday" || $day_of_week == "Thursday") {
    echo "<br><div class=\"list-dateline\">Submissions received since Tuesday at noon</div>\n";
    $query = "SELECT * FROM articles WHERE date > DATETIME('NOW','LOCALTIME','WEEKDAY 2','-7 DAYS','START OF DAY','+12 HOURS') ORDER BY count DESC, date DESC";
@@ -44,8 +44,10 @@ else {
    $query = "SELECT * FROM articles WHERE date > DATETIME('NOW','LOCALTIME','WEEKDAY 4','-7 DAYS','START OF DAY','+12 HOURS') ORDER BY count DESC, date DESC";
 }
 
+// Execute query
 $result = $db->query($query) or die("Error retrieving articles");
 
+// Display results
 if($result->numRows() > 0) {
     echo "<dl>\n";
     while($row = $result->fetch()) {
@@ -71,6 +73,7 @@ else {
     echo "No articles";
 }
 
+// Close page and database
 echo "</div></div>\n";
 
 unset ($db);
--- website/public/old.php
+++ website/public/old.php
@@ -1,5 +1,11 @@
 <?php
 
+// Set path to arxivPicks files
+$APHOME = "/path/to/arxivPicks"
+$db_file = $APHOME."/backend/arxivPicks.db";
+$logfile = $APHOME."/website/log.txt";
+
+// Write html for display
 echo "<html>\n";
 echo "<head>\n";
 echo "<title>arXiv Picks</title>\n";
@@ -13,19 +19,17 @@ echo "</div>\n";
 echo "<div id=\"content\"><div id=\"abs\"><div class=\"subheader\">\n";
 echo "<h1><a href=\"http://arxiv.org/list/astro-ph/new?skip=0&show=250\">astro-ph</a> | <a href=\"index.php\">New papers</a></h1>\n";
 echo "</div></div>\n";
-
 echo "<div id=\"dlpage\">\n";
-
 echo "<br><div class=\"list-dateline\">All submissions received</div>\n";
 
-// set path of database file
-$db_file = "/insert/path/to/database/arxiv.db";
-// open database object
+// Open database object
 $db = new SQLiteDatabase($db_file) or die("Could not open database");
 
+// Query all articles from database
 $query = "SELECT * FROM articles ORDER BY date DESC";
 $result = $db->query($query) or die("Error retrieving articles");
 
+// Display results
 if($result->numRows() > 0) {
     echo "<dl>\n";
     while($row = $result->fetch()) {
@@ -51,6 +55,7 @@ else {
     echo "No articles";
 }
 
+// Close page and database
 echo "</div></div>\n";
 
 unset ($db);
