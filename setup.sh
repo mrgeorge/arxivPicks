d39faa08131508606077f15260d79ad3bcb89e57
Matt George <mattgeorge1@gmail.com>
2014-02-23 21:09:48 -0800

    add instructions to setup.sh for finishing install, fix find lines

--- setup.sh
+++ setup.sh
@@ -12,17 +12,17 @@ cd -
 chmod o+w $APHOME $APHOME/backend $APHOME/backend/arxivPicks.db
 
 # Replace paths in php scripts
-find -path $APHOME/*.php -type f -exec sed -i "s%/path/to/arxivPicks%${APHOME}%g" {} \;
+find $APHOME/ -name \*.php -type f -exec sed -i "s%/path/to/arxivPicks%${APHOME}%g" {} \;
 
 # Prompt for URL
 read -e -p "Enter URL for public website: " -i "http://example.com/arxivPicks" APURL
 # Replace URL in browser extension
-find -path $APHOME/extension/*.user.js -type f -exec sed -i "s%http://example.com%${APURL}%g" {} \;
+find $APHOME/extension/*.user.js -type f -exec sed -i "s%http://example.com%${APURL}%g" {} \;
 
 # Prompt for email address
 read -e -p "Enter email address for discussion list: " -i "everyone@example.com" APEMAIL
 # Replace address in mail script
-find -path $APHOME/backend/*mail.php -type f -exec sed -i "s%everyone@example.com%${APEMAIL}%g" {} \;
+find $APHOME/backend/*mail.php -type f -exec sed -i "s%everyone@example.com%${APEMAIL}%g" {} \;
 
 # Prompt for local web dir
 read -e -p "Enter path to local dir for public web files: " -i "$HOME/public_html/arxivPicks" APWEBDIR
@@ -32,3 +32,12 @@ fi
 # Move public web files to web dir
 rsync -avz $APHOME/website/public/ $APWEBDIR
 rm -rf $APHOME/website/public/*
+
+echo "================================================================================"
+echo "arxivPicks setup has run."
+echo "Database initialized in ${APHOME}/backend/arxivPicks.db"
+echo "Web files moved to ${APWEBDIR} with assumed URL ${APURL}"
+echo "To do: -add cron job to run mail scripts in ${APHOME}/backend"
+echo "       -install user scripts in ${APHOME}/extension with Greasemonkey (Firefox) or Greasekit/SIMBL (Safari)"
+echo "        or convert to .xpi (Firefox) at https://arantius.com/misc/greasemonkey/script-compiler.php"
+echo "================================================================================"
